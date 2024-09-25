90a92335f0f67cb79f1743bead9ac096ddde1738
socratic617 <erik.solis02135@gmail.com>
2024-09-24 22:48:00 -0400

    Add S3 file upload functionality using boto3 - Import necessary modules and handle ImportError for boto3-stubs. - Create a session and S3 client using boto3. - Implement file upload to an S3 bucket with sample content ("Hello, World!").

--- src/files_api/s3.py
+++ src/files_api/s3.py
@@ -1,8 +1,23 @@
 import boto3
 
+try: 
+    from mypy_boto3_s3 import S3Client
+    from mypy_boto3_s3.type_defs import (
+        PutObjectOutputTypeDef,
+        ResponseMetadataTypeDef,
+    )
+except ImportError: 
+    print("boto3-stubs[s3] is not installed")
+
 BUCKET_NAME = "cloud-course-bucket-erik"
 
-s3_client = boto3.client("s3")
+session = boto3.Session()
+s3_client: "S3Client" = session.client("s3")
 
 # write a file to the s3 bucket with the contents "Hello, World!"
-s3_client.put_object(Bucket=BUCKET_NAME, Key="folder/hello.txt", Body="Hello, World!")
+response: "PutObjectOutputTypeDef" = s3_client.put_object(
+    Bucket=BUCKET_NAME,
+    Key="folder/hello.txt",
+    Body="Hello, World!",
+    ContentType="text/plain",
+)
