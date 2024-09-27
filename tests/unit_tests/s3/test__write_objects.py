41d0ea7d0b97af26ec9e85eadab8724280b4acf2
socratic617 <erik.solis02135@gmail.com>
2024-09-27 05:06:00 -0400

    Add unit test for S3 object upload and validation; run with make test

---
+++ tests/unit_tests/s3/test__write_objects.py
@@ -0,0 +1,33 @@
+import boto3
+
+from files_api.s3.write_objects import upload_s3_object
+
+TEST_BUCKET_NAME = "mlops-unit-test-bucket"
+
+def test__upload_s3_object():
+    # create an s3 bucket
+    s3_client = boto3.client("s3")
+    s3_client.create_bucket(Bucket=TEST_BUCKET_NAME)
+
+    # upload a file to the bucket, with a particular content type
+    object_key = "test.txt"
+    file_content: bytes = b"Hello, world!"
+    content_type = "text/plain"
+    upload_s3_object(
+        bucket_name=TEST_BUCKET_NAME,
+        object_key=object_key,
+        file_content=file_content,
+        content_type=content_type,
+        s3_client=s3_client,
+    )
+
+    # check that the file was uploaded with the correct content type 
+    response = s3_client.get_object(Bucket=TEST_BUCKET_NAME, Key=object_key)
+    assert response["ContentType"] == content_type
+    assert response["Body"].read() == file_content
+
+    # clean up by deleting the bucket
+    response = s3_client.list_objects_v2(Bucket=TEST_BUCKET_NAME)
+    for obj in response.get("Contents", []):
+        s3_client.delete_object(Bucket=TEST_BUCKET_NAME, Key=obj["Key"])
+    s3_client.delete_bucket(Bucket=TEST_BUCKET_NAME)
